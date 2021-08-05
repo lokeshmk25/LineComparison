478c568380992df44a462d49f9bc6bb15c7eac77
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-05 20:26:35 +0530

    Added:length of line

--- src/com/bridgelabz/Linecomparison.java
+++ src/com/bridgelabz/Linecomparison.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Linecomparison {
     public static void main(String[] args) {
         System.out.println("welcome to line comparison");
+        Scanner xy = new Scanner(System.in);
+        System.out.println("enter the numbers");
+        int x1=xy.nextInt();
+        int x2=xy.nextInt();
+        int y1=xy.nextInt();
+        int y2=xy.nextInt();
+
+        double lengthofline = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println(lengthofline);
+
     }
 }
